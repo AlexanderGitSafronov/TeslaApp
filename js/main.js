73cf1455acf87fc89825d95e77bef4036cfae5b2
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-03-27 11:53:32 +0300

    upgrade

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function rangeSlider(value) {
+    document.querySelector('.rangeValue').innerHTML = value + '&deg C';
+    document.querySelector('.rangeValue_2').innerHTML = value + '&deg C';
+  }
+
+
+
